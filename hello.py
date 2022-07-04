0500112d7c8817877bb5c8005d043021d89dc3c9
haohaoj <75346953+haohaoj@users.noreply.github.com>
2022-07-04 16:34:32 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello from Binder!")
+
+number1 = input("Type your first number: ")
+print("Your first number is: ", number1)
+
+number2 = input("Type your second number: ")
+print("Your second number is: ", number2)
+
+sum1 = int(number1) + int(number2)
+print("The result of number 1+ number 2 is: ",sum1)
